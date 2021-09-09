04b4bf6d6acd3491f1b016bff309f8b357195964
Ty Minnick <tmm8501@rit.edu>
2021-09-09 17:04:35 -0400

    Added Phase 1 program

---
+++ Phase1.py
@@ -0,0 +1,12 @@
+"""
+@Authors: Ty Minnick. Mike Dortz, Maham Imtiaz, Deryke Tang
+Description: This file contains preliminary data mining for phase 1 of the project.
+Date: 9/9/2021
+"""
+
+
+def main():
+    print("hello")
+
+
+main()

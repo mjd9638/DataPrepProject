0f33536c4e1d96b1108b8c225b17ad9791c51dc3
Maham <mi3555@rit.edu>
2021-09-09 17:25:34 -0400

    Pulled tsv files into dataframes using pandas

--- Phase1.py
+++ Phase1.py
@@ -4,9 +4,25 @@ Description: This file contains preliminary data mining for phase 1 of the proje
 Date: 9/9/2021
 """
 
+import sys
+import pandas
 
-def main():
-    print("hello")
+
+def main(argv):
+    # name_basics_df = pandas.read_csv("name.basics.tsv", sep='\t')
+    # title_akas_df = pandas.read_csv("title.akas.tsv", sep='\t', dtype={'titleId': 'str','ordering': 'int',
+    #                                                                    'title': 'str', 'region': 'str',
+    #                                                                    'language': 'str', 'types': 'str',
+    #                                                                    'attributes': 'str', 'isOriginalTitle': 'str'})
+    # title_basics_df = pandas.read_csv("title.basics.tsv", sep='\t', dtype={'tconst': 'str', 'titleType': 'str',
+    #                                                                        'primaryTitle': 'str',
+    #                                                                        'originalTitle': 'str', 'isAdult': 'str',
+    #                                                                        'startYear': 'str', 'endYear': 'str',
+    #                                                                        'runtimeMinutes': 'str', 'genres': 'str'})
+    title_ratings_df = pandas.read_csv("title.ratings.tsv", sep='\t')
 
 
-main()
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+

28f720476eacf627c0503f268473aa73395660fd
Ty Minnick <tmm8501@rit.edu>
2021-09-10 17:10:24 -0400

    Reduced the database size and added functionality to split attributes with multiple values

--- Phase1.py
+++ Phase1.py
@@ -20,16 +20,43 @@ def merge_databases(names, akas, basics, ratings):
     names = names.explode('knownForTitles')
     names.rename(columns={'knownForTitles': 'tconst'}, inplace=True)
     final_df = pd.merge(final_df, names, on='tconst', how='left')
+    print(final_df.columns, '\n')
     print(final_df.head())
+    final_df.to_csv('final.tsv', sep='\t')
+
+
+def split_attributes(dataset):
+    attributes = ['types', 'attributes', 'genres', 'primaryProfession']
+    for each in attributes:
+        dataset[each] = dataset[each].str.split(',')
+        dataset[each] = dataset.explode(each)
+    print(dataset.head())
+    dataset.to_csv('final.tsv', sep='\t')
 
 
 def main(argv):
     #pd.set_option('display.max_columns', None)
-    name_basics_df = pd.read_csv("name.basics.tsv", sep='\t', dtype=str, converters={'knownForTitles': lambda x: x.split(',')})
-    title_akas_df = pd.read_csv("title.akas.tsv", sep='\t', dtype=str)
-    title_basics_df = pd.read_csv("title.basics.tsv", sep='\t', dtype=str)
-    title_ratings_df = pd.read_csv("title.ratings.tsv", sep='\t')
-    merge_databases(name_basics_df, title_akas_df, title_basics_df, title_ratings_df)
+    # read in names data set
+    print('Reading in names...')
+    #name_basics_df = pd.read_csv("name.basics.tsv", sep='\t', dtype=str, converters={'knownForTitles': lambda x: x.split(',')})
+    # read in akas data set
+    print('Reading in akas...')
+    #title_akas_df = pd.read_csv("title.akas.tsv", sep='\t', dtype=str)
+    # read in basics data set
+    print('Reading in basics...')
+    #title_basics_df = pd.read_csv("title.basics.tsv", sep='\t', dtype=str)
+    # read in ratings data set
+    print('Reading in ratings...')
+    #title_ratings_df = pd.read_csv("title.ratings.tsv", sep='\t')
+    # remove all foreign entries from akas data set
+    #title_akas_df = title_akas_df[title_akas_df['region'] == 'US']
+    # remove all non-movie entries from basics data set
+    #title_basics_df = title_basics_df[(title_basics_df['titleType'] == 'movie') | (title_basics_df['titleType'] == 'tvMovie')]
+    #merge_databases(name_basics_df, title_akas_df, title_basics_df, title_ratings_df)
+    print('Reading in Final...')
+    final = pd.read_csv("final.tsv", sep='\t', dtype=str)
+    print('Splitting the data...')
+    split_attributes(final)
 
 
 if __name__ == '__main__':
